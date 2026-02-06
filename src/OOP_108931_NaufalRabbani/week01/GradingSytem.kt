5901a98b40055e6602625b70a10c1e030a56e005
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-02-06 11:14:26 +0700

    week01: implement grading logic using when expression

--- src/OOP_108931_NaufalRabbani/week01/GradingSytem.kt
+++ src/OOP_108931_NaufalRabbani/week01/GradingSytem.kt
@@ -1,9 +1,16 @@
 package OOP_108931_NaufalRabbani.week01
 
 fun main(){
-    var name: String = "Nopal"
-    var score: Int = 80
+    val name = "Nopal"
+    val score = 80
 
         println("nama $name dan nilainya $score")
 
+    val grade = when(score){
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else -> "D"
+    }
+    println("Grade Anda adalah $grade")
 }
