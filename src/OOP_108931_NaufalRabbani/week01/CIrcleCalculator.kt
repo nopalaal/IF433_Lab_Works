20dd65a4e604ff3f15195201c58f16758fe88b3f
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-02-12 17:54:30 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/OOP_108931_NaufalRabbani/week01/CIrcleCalculator.kt
+++ src/OOP_108931_NaufalRabbani/week01/CIrcleCalculator.kt
@@ -1,16 +1,13 @@
 package OOP_108931_NaufalRabbani.week01
 
-fun main(args: Array<String>) {
-// Variable definition
+fun main() {
     val radius = 7.0
     val pi = 3.14
-// Calculation
     val area: Double = pi * radius * radius
 
-// Output Concatenation
 println("Radius: $radius Area: $area")
-// Logic check
-println(checkSize(area))
 
+    println(checkSize(area))
 }
+
 fun checkSize(area: Double) = if (area > 100) "Large Circle" else "Small Circle"
