095aedf2c2f0856ae8c00f7d141c5e5beae1c025
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-02-12 18:02:53 +0700

    week01: (tugas 2) implement discount logic

--- src/OOP_108931_NaufalRabbani/week01/GameStore.kt
+++ src/OOP_108931_NaufalRabbani/week01/GameStore.kt
@@ -3,4 +3,10 @@ package OOP_108931_NaufalRabbani.week01
 fun main(){
     val gameTitle: String? = null
     val price: Int? = null
+
+}
+
+fun calculateDiscount(price: Int): Int = when{
+    price > 500000 -> (price * 0.20).toInt()
+    else -> (price * 0.10).toInt()
 }
