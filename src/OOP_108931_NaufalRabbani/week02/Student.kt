63f0168c73d881aa08d3928da4aba774eb2f756a
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-02-13 10:49:26 +0700

    week02: add warning validation in init block

--- src/OOP_108931_NaufalRabbani/week02/Student.kt
+++ src/OOP_108931_NaufalRabbani/week02/Student.kt
@@ -1,9 +1,17 @@
 package OOP_108931_NaufalRabbani.week02
 
 class Student(
-    val student: String,
+    val name: String,
     val nim: String,
     val major: String
 ){
-
+    init {
+        if(nim.length !=5){
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin bermasalah di sistem")
+        }
+        else{
+            println("LOG : Objek Student $name berhasil dialokasikan ke memory")
+        }
+    }
 }
