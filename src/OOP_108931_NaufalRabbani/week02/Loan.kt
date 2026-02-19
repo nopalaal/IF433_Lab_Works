8fac53a345bbc2ae3c94f3f6a40ab922402a8cb1
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-02-19 18:46:31 +0700

    week02: (task 1) create Loan class with default args

---
+++ src/OOP_108931_NaufalRabbani/week02/Loan.kt
@@ -0,0 +1,9 @@
+package OOP_108931_NaufalRabbani.week02
+
+class Loan (
+    val bookTitle: String,
+    val borrower: String,
+    val loanDuration: Int = 1
+) {
+    fun  calculateFine() : Int = if(loanDuration > 3) (loanDuration - 3) * 2000 else 0
+}
