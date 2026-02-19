1152f1a1f4c777706ce84edd6556c1e12d942e9e
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-02-19 18:24:13 +0700

    week02: implement selection logic for constructors

--- src/OOP_108931_NaufalRabbani/week02/Main.kt
+++ src/OOP_108931_NaufalRabbani/week02/Main.kt
@@ -5,7 +5,7 @@ import java.util.Scanner
 fun main(){
     val scanner = Scanner(System.`in`)
 
-    print("--- APLIKASI PMB UMN---")
+    print("--- APLIKASI PMB UMN--- \n")
 
     print("Masukkan nama anda: ")
     val name = scanner.nextLine()
@@ -25,5 +25,22 @@ fun main(){
         val s1 = Student(name, nim, major)
 
         println("Status: Pendaftaran Berhasil")
+    }
+
+    print("Pilih Jalur(1. Reguler, 2. Umum): ")
+    val type = scanner.nextInt()
+    scanner.nextLine()
+
+    if(type == 1){
+        println("Masukkan Jurusan : ")
+        val major = scanner.nextLine()
+        val s1 = Student(name, nim, major)
+        println("Terdaftar di : {${s1.major} dengan GPA awal {${s1.gpa}}")
+    }else if( type == 2){
+        val s2 = Student(name, nim)
+        println("Terdaftar di : ${s2.major} dengan GPA awal ${s2.gpa}")
+    }else{
+        print("Aneh, pendaftarannya tidak valid")
+
     }
 }
