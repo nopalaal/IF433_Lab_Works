9a97cf30d842868fec4db563e49e08bb93779167
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-02-20 11:30:19 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/OOP_108931_NaufalRabbani/Week03/weapon.kt
@@ -0,0 +1,21 @@
+package OOP_108931_NaufalRabbani.Week03
+
+class weapon(
+    val name: String,
+) {
+    var damage: Int = 0
+        set(value) {
+            if(value < 0){
+                println("damage ga bole mines")
+            }else if(value > 1000){
+                println("damage terlalu OP")
+                field = 1000
+            }else{
+                println("damage sukses di set")
+                field = value
+                }
+            }
+
+        val tier: String
+        get() = if (damage > 800) "Legendaray" else if(damage > 500) "Epic" else "Common"
+}
