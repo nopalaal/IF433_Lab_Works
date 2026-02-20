030e1bd21383b80c6248d1718004ab67b4047d1c
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-02-20 11:14:15 +0700

    week03: cleanup code

--- src/OOP_108931_NaufalRabbani/Week03/main.kt
+++ src/OOP_108931_NaufalRabbani/Week03/main.kt
@@ -3,7 +3,6 @@ package OOP_108931_NaufalRabbani.Week03
 fun main(){
     val e = Employee("Asep")
     println("test error")
-    e.salary = -1000
     e.salary = 5000
     println("gaji: ${e.salary}")
 
