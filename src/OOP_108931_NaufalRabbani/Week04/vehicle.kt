bf5070d2c2d6f668c9f4225ee968cf184b7266f9
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-02-27 10:45:51 +0700

    week04: create basic Vehicle parent class

---
+++ src/OOP_108931_NaufalRabbani/Week04/vehicle.kt
@@ -0,0 +1,14 @@
+package OOP_108931_NaufalRabbani.Week04
+
+open class vehicle(val brand: String) {
+    var speed: Int = 0
+
+    open fun accelerate() {
+        speed += 10
+        println("$brand melaju dengan kecepatan $speed km/jam")
+    }
+
+    open fun honk(){
+        println("beep beep!")
+    }
+}
