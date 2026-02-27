69eb880500c30cd5245ef19a4fde2b76515d67b8
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-02-27 10:50:49 +0700

    week04: create Car class extending Vehicle

---
+++ src/OOP_108931_NaufalRabbani/Week04/Car.kt
@@ -0,0 +1,8 @@
+package OOP_108931_NaufalRabbani.Week04
+
+open class Car(brand: String, val numberOfDoors: Int) : vehicle(brand) {
+    fun openTrunk(){
+        println("Bagasi mobil $brand dengan $numberOfDoors pintu terbuka")
+    }
+
+}
