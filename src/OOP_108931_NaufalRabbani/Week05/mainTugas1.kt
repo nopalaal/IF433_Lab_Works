c677eadbb827fcf68200be1091310c801cb94423
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-03-06 10:54:46 +0700

    week05: (task 1) test MathHelper overloading in main

---
+++ src/OOP_108931_NaufalRabbani/Week05/mainTugas1.kt
@@ -0,0 +1,12 @@
+package OOP_108931_NaufalRabbani.Week05
+
+fun main(){
+    val mathHelper = MathHelper()
+    val result1 = mathHelper.hitungLuas(5)
+    val result2 = mathHelper.hitungLuas(13, 12)
+    val result3 = mathHelper.hitungLuas(7.5)
+
+    println("hasil luas pesergi : $result1")
+    println("hasil luas pesergi panjang : $result2")
+    println("hasil luas lingkaran : $result3")
+}
