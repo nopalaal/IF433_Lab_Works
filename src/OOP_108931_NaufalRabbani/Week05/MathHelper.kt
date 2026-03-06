7762790404219552896248f4b947f69c653fa3a5
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-03-06 10:51:10 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/OOP_108931_NaufalRabbani/Week05/MathHelper.kt
@@ -0,0 +1,13 @@
+package OOP_108931_NaufalRabbani.Week05
+
+class MathHelper(){
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+     fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+}
