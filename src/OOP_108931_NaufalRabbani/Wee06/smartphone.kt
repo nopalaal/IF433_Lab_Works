f8a4918a9c3a7a889e4043009c589e624f987a10
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-03-19 15:49:03 +0700

    week06: fix diamond problem using explicit super delegation

--- src/OOP_108931_NaufalRabbani/Wee06/smartphone.kt
+++ src/OOP_108931_NaufalRabbani/Wee06/smartphone.kt
@@ -1,5 +1,9 @@
 package OOP_108931_NaufalRabbani.Wee06
 
 class smartphone : camera, phone{
-
+    override fun turnOn() {
+        super<camera>.turnOn()
+        super<phone>.turnOn()
+        println("sistem operasi smartphone berhasil di booting")
+    }
 }
