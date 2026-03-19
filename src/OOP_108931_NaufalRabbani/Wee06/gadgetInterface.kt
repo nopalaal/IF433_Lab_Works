a53a5183510b79b1d09b0ccaf7d8de0d274d905c
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-03-19 15:46:32 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/OOP_108931_NaufalRabbani/Wee06/gadgetInterface.kt
@@ -0,0 +1,8 @@
+package OOP_108931_NaufalRabbani.Wee06
+
+interface camera {
+    fun turnOn(){ println("lensa kamera terbuka dan sensor aktif") }
+}
+interface phone {
+    fun turnOn(){ println("layar menyala dan siap digunakan") }
+}
--- src/OOP_108931_NaufalRabbani/Wee06/main.kt
+++
@@ -1,5 +0,0 @@
-package OOP_108931_NaufalRabbani.Wee06
-
-fun main(){
-    val watch = swa
-}
