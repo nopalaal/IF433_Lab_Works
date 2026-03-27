d1f1cd5b52cd67cc9a635e9c70097d6edf7a5136
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-03-27 11:30:49 +0700

    week07: (trial) demonstrate regular class equality failure

--- src/OOP_108931_NaufalRabbani/Week07/Main.kt
+++ src/OOP_108931_NaufalRabbani/Week07/Main.kt
@@ -9,4 +9,10 @@ fun main() {
     println("\n=== TEST COMPANION OBJECT ===")
     val client = NetworkClient.createClient()
     client.connect()
+
+    println("\n === TEST REGULAR CLASS === ")
+    val reg1 = RegularUser("Budi",21)
+    val reg2 = RegularUser("Siti",25)
+    println(reg1)
+    println("sama? ${reg1 == reg2}")
 }
---
+++ src/OOP_108931_NaufalRabbani/Week07/UserModels.kt
@@ -0,0 +1,3 @@
+package OOP_108931_NaufalRabbani.Week07
+
+class RegularUser(val name:String, val age: Int)
