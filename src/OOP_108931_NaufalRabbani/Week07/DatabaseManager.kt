facfbb752b264a35d3e8b3a05d78917e8a096024
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-03-27 11:15:02 +0700

    week07: implement companion object as factory in NetworkClient

--- src/OOP_108931_NaufalRabbani/Week07/DatabaseManager.kt
+++ src/OOP_108931_NaufalRabbani/Week07/DatabaseManager.kt
@@ -1,6 +1,8 @@
 package OOP_108931_NaufalRabbani.Week07
 
 object DatabaseManager{
+        const val databaseUrl: String = "jdbc:mysql://localhost:3306/mydatabase"
+
     var connectionStatus: String = "Disconnected"
 
     fun connect(){
--- src/OOP_108931_NaufalRabbani/Week07/NetworkClient.kt
+++ src/OOP_108931_NaufalRabbani/Week07/NetworkClient.kt
@@ -1,6 +1,14 @@
 package OOP_108931_NaufalRabbani.Week07
 
 class NetworkClient private constructor(val url: String) {
+  companion object {
+      const val BASE_URL = "https://api.umn.ac.id"
+
+      fun createClient(): NetworkClient {
+          println("membangun NetworkClient dengan URL $BASE_URL")
+          return NetworkClient(BASE_URL)
+      }
+  }
     fun connect() {
         println("Connecting to $url")
     }
