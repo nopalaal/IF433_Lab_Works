505e5481d84dfb3c647a4216f79e7d8ce98f0c71
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-03-27 11:37:08 +0700

    week07: create ApiResponse sealed class

--- src/OOP_108931_NaufalRabbani/Week07/SystemState.kt
+++ src/OOP_108931_NaufalRabbani/Week07/SystemState.kt
@@ -3,3 +3,9 @@ package OOP_108931_NaufalRabbani.Week07
 enum class AppState {
     STARTING, RUNNING, STOPPED
 }
+
+sealed class ApiResponse {
+    data class Success(val data: String) : ApiResponse()
+    data class Error(val message: String) : ApiResponse()
+    object Loading : ApiResponse()
+}
