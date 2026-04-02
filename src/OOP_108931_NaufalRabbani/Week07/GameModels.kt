f3e977ee7c84a980ec2a6bc4b2ea7617b413d07d
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-04-02 18:15:36 +0700

    week07: (task) create GameItem data class

--- src/OOP_108931_NaufalRabbani/Week07/GameModels.kt
+++ src/OOP_108931_NaufalRabbani/Week07/GameModels.kt
@@ -4,3 +4,4 @@ enum class ItemRarity(val dropChance: Int) {
     COMMON(70), UNCOMMON(50), RARE(30), EPIC(10), LEGENDARY(1)
 }
 
+data class GameItem(val name: String, val damage: Int, val rarity: ItemRarity )
