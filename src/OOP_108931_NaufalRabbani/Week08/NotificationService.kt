2f186c3343e47850176cfd45276d5b39a6c31bfe
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-04-17 11:06:13 +0700

    week08: implement smart cast in function

---
+++ src/OOP_108931_NaufalRabbani/Week08/NotificationService.kt
@@ -0,0 +1,15 @@
+package OOP_108931_NaufalRabbani.Week08
+
+class NotificationService{
+    fun sendEmail(emailAddress: String){
+        println("Sending email to $emailAddress")
+    }
+
+    fun processUser(user: UserProfile){
+        if(user.email != null){
+            sendEmail(user.email
+        } else {
+            println("No email address provided for ${user.name}"))
+        }
+    }
+}
