569faa6ee3a4f7b028258ed0e7210576ce864d5b
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-04-23 17:07:53 +0700

    week08: (task) implement payment processing with !!

--- src/OOP_108931_NaufalRabbani/Week08/ApiParser.kt
+++ src/OOP_108931_NaufalRabbani/Week08/ApiParser.kt
@@ -1,22 +1,39 @@
 package OOP_108931_NaufalRabbani.Week08
 
-class ApiParser()
-fun parseProduct(rawJson: Map<String, Any?>): Product? {
-    val id   = requireNotNull(rawJson["id"])   { "API Invalid: Missing ID" }
-    val name = requireNotNull(rawJson["name"]) { "API Invalid: Missing Name" }
+class ApiParser{
+    fun parseProduct(rawJson: Map<String, Any?>): Product? {
+        val id   = requireNotNull(rawJson["id"])   { "API Invalid: Missing ID" }
+        val name = requireNotNull(rawJson["name"]) { "API Invalid: Missing Name" }
 
-    val type = rawJson["type"] as? String
+        val type = rawJson["type"] as? String
 
-    return when (type) {
-        "ELECTRONIC" -> {
-            val warranty = rawJson["warranty"] as? Int ?: 12
-            Product.Electronics(id = id.toString(), name = name.toString(), warrantyMonths = warranty)
+        return when (type) {
+            "ELECTRONIC" -> {
+                val warranty = rawJson["warranty"] as? Int ?: 12
+                Product.Electronics(id = id.toString(), name = name.toString(), warrantyMonths = warranty)
+            }
+            "CLOTHING" -> {
+                val size = rawJson["size"] as? String ?: "All Size"
+                Product.Clothing(id = id.toString(), name = name.toString(), size = size)
+            }
+            else -> null
         }
-        "CLOTHING" -> {
-            val size = rawJson["size"] as? String ?: "All Size"
-            Product.Clothing(id = id.toString(), name = name.toString(), size = size)
-        }
-        else -> null
     }
+
+    fun checkout(product: Product) {
+        val id = when (product) {
+            is Product.Electronics -> product.id
+            is Product.Clothing   -> product.id
+        }
+
+        val transactionId = JavaPaymentService.ProcessPayment(id)!!
+        println("Transaction ID: $transactionId")
+    }
+
 }
+
+
+
+
+
 
