28a1598b8d9358ebcf18ea7d60fbafbd09a580fd
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-04-23 17:11:55 +0700

    week08: (task) process mixed raw JSON list using as?

---
+++ src/OOP_108931_NaufalRabbani/Week08/EcommerceMain.kt
@@ -0,0 +1,23 @@
+package OOP_108931_NaufalRabbani.Week08
+
+fun main(){
+    val rawApiData: List<Map<String, Any?>> = listOf(
+        mapOf("id" to "E01", "name" to "Laptop", "type" to "ELECTRONIC", "warranty" to 24),
+        mapOf("id" to "C01", "name" to "T-Shirt", "type" to "CLOTHING", "size" to "XL"),
+        mapOf("id" to "E02", "name" to "Mouse", "type" to "ELECTRONIC", "warranty" to "Not An Integer"), // Corrupted warranty
+        mapOf("name" to "Ghost Item", "type" to "CLOTHING"), // Missing ID!
+        mapOf("id" to "X01", "name" to "Unknown", "type" to "FOOD") // Unknown type
+    )
+
+    val parser = ApiParser()
+
+    for (raw in rawApiData) {
+        try {
+            val product = parser.parseProduct(raw)
+            product?.let { parser.checkout(it) } ?: println(" unsupported type for data -> $raw")
+        } catch (e: IllegalArgumentException) {
+            println("Corrupted data detected ${e.message}")
+        }
+    }
+
+}
