4922d10fe49786b3a4528e5bf362e09846dd60f7
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-04-23 17:02:23 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/OOP_108931_NaufalRabbani/Week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package OOP_108931_NaufalRabbani.Week08;
+
+public class JavaPaymentService {
+    public static String ProcessPayment(String productId){
+        return "TRX-" + productId + " -SUCCESS";
+    }
+}
