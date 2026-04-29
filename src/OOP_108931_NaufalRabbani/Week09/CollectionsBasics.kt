b7206cda0515f1b3bf1a18b635e464208d32c6e8
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-04-29 19:34:11 +0700

    week09: implement map for key-value storage

--- src/OOP_108931_NaufalRabbani/Week09/CollectionsBasics.kt
+++ src/OOP_108931_NaufalRabbani/Week09/CollectionsBasics.kt
@@ -3,13 +3,6 @@ package OOP_108931_NaufalRabbani.Week09
 fun main(){
     println("===  TEST LIST ===")
 
-    val uniqueNumbers = setOf(1,2,2,3,3,4)
-    println("Unique numbers in unique: $uniqueNumbers")
-
-    val activeUsers = mutableSetOf("UserA", "UserB")
-    activeUsers.add("UserC")
-    activeUsers.add("UserA")
-    println("Active users: $activeUsers")
 
     val frameworks: List<String> = listOf("Spring", "Java", "C++")
 
@@ -19,4 +12,22 @@ fun main(){
     scores.add(95)
     scores[0] = 88
     println("Mutable List: $scores")
+
+    println("==== SET LIST====")
+    val uniqueNumbers = setOf(1,2,2,3,3,4)
+    println("Unique numbers in unique: $uniqueNumbers")
+
+    val activeUsers = mutableSetOf("UserA", "UserB")
+    activeUsers.add("UserC")
+    activeUsers.add("UserA")
+    println("Active users: $activeUsers")
+
+    println("=== MAP LIST ===")
+    val studentGrades = mapOf("Alice" to "A", "Bob" to "B", "Charlie" to "C")
+    println("Student Grades: $studentGrades")
+
+    val inventory = mutableMapOf("Apple" to 50, "Banana" to 30)
+    inventory["Orange"] = 20
+    inventory["Apple"] = 45
+    println("Inventory: $inventory")
 }
