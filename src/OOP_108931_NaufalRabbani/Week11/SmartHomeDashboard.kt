950f2650c39b470af020697dce6d3014404007c9
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-05-14 20:34:28 +0700

    week11: (task) implement device search with let

--- src/OOP_108931_NaufalRabbani/Week11/SmartHomeDashboard.kt
+++ src/OOP_108931_NaufalRabbani/Week11/SmartHomeDashboard.kt
@@ -28,4 +28,9 @@ fun main(){
 
     homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
 
+    val searchResult = homeDevices.find { it.category == "Camera" }
+    searchResult?.let {
+        println("\n=== Hasil Pencarian ===")
+        println(it.diagnose())
+    }
 }
