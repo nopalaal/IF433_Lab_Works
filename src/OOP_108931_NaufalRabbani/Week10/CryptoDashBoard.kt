ba999bd0adbe57616dfa5f61f45d33f5be4c58a1
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-05-14 19:33:11 +0700

    week10: (task) initialize coin repository

---
+++ src/OOP_108931_NaufalRabbani/Week10/CryptoDashBoard.kt
@@ -0,0 +1,5 @@
+package OOP_108931_NaufalRabbani.Week10
+
+fun main(){
+    val coinRepo = WalletRepository<Coin>()
+}
---
+++ src/OOP_108931_NaufalRabbani/Week10/WalletModels.kt
@@ -0,0 +1,4 @@
+package OOP_108931_NaufalRabbani.Week10
+
+data class Coin(val name: String, val balance: Double)
+data class Transcation(val id: String, val amount: Double)
