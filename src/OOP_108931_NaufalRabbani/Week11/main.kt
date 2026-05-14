082746c65e9547773c4cb5f2a8b53baa6ccf1978
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-05-14 20:08:29 +0700

    week11: test extension functions in main

---
+++ src/OOP_108931_NaufalRabbani/Week11/main.kt
@@ -0,0 +1,11 @@
+package OOP_108931_NaufalRabbani.Week11
+
+fun main(){
+println("TEST EXTENSION")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+
+    println("apakah null? ${text.isNullOrEmptyCustom()}")
+}
