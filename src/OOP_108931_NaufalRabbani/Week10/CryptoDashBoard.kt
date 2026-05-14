033dea895f0dd764fb881ac18335600837c0da82
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-05-14 19:38:23 +0700

    week10: (task) populate coin repository

--- src/OOP_108931_NaufalRabbani/Week10/CryptoDashBoard.kt
+++ src/OOP_108931_NaufalRabbani/Week10/CryptoDashBoard.kt
@@ -2,4 +2,11 @@ package OOP_108931_NaufalRabbani.Week10
 
 fun main(){
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.addItem(Coin("BTC",5.0))
+    coinRepo.addItem(Coin("ETH",10.0))
+    coinRepo.addItem(Coin("USDT",1.0))
+
+
+
 }
