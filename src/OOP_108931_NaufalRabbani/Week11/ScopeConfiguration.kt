e47f3e40637de99a526933a79ce295b92f40cf38
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-05-14 20:15:15 +0700

    week11: implement apply for object initialization

--- src/OOP_108931_NaufalRabbani/Week11/ScopeConfiguration.kt
+++ src/OOP_108931_NaufalRabbani/Week11/ScopeConfiguration.kt
@@ -1,3 +1,13 @@
 package OOP_108931_NaufalRabbani.Week11
 
 data class user(var name: String = " ", var age: Int = 0)
+
+fun main(){
+    println("TEST APPLY")
+    val user = user().apply {
+        name = "AleX"
+        age = 17
+
+    }
+    println(user)
+}
