d96cfd6ee7205d7dbe697bcb41fb0b8cd2a18e86
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-05-15 10:31:22 +0700

    week12: implement throw keyword for validation

--- src/OOP_108931_NaufalRabbani/Week12/CustomException.kt
+++ src/OOP_108931_NaufalRabbani/Week12/CustomException.kt
@@ -1,3 +1,17 @@
 package OOP_108931_NaufalRabbani.Week12
 
-class InsufficientBalanceException(val amount: Int, val balance: Int) : Exception("Attemped $amount, balance is only $balance")
+class InsufficientBalanceException(val amount: Double, val balance: Double) : Exception("Attemped $amount, balance is only $balance")
+
+class BankAccount(var balance: Double) {
+    fun withdraw(amount: Double){
+        if (amount < 0){
+            throw IllegalArgumentException("Amount must be positive")
+        }
+        if(amount > balance){
+            throw InsufficientBalanceException(amount,balance)
+        }
+        balance -= amount
+        println("Withdraw Successful. Remaining balance: $balance")
+    }
+
+}
