b921c72940f7c0a0195e8770271bbe323c3bdf51
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-05-15 10:46:26 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/OOP_108931_NaufalRabbani/Week12/SmartFeeder.kt
@@ -0,0 +1,7 @@
+package OOP_108931_NaufalRabbani.Week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int{
+    require(requestedGram > 1){
+        "porsi Kibble harus lebih dari 0 gr"
+    }
+}
