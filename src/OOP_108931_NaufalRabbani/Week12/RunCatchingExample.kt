be8beffc3acced52ce46dd2b06ae471ad1050836
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-05-15 10:37:41 +0700

    week12: handle Result using getOrElse and recover

--- src/OOP_108931_NaufalRabbani/Week12/RunCatchingExample.kt
+++ src/OOP_108931_NaufalRabbani/Week12/RunCatchingExample.kt
@@ -3,4 +3,10 @@ package OOP_108931_NaufalRabbani.Week12
 fun main(){
     println("TES RUNCACHING")
     val result: Result<Int> = runCatching { "42X".toInt() }
+
+    val safeValue = result.getOrElse {-1}
+    println("safeValue: $safeValue")
+
+    val recovered = result.recover{0}.getOrNull()
+    println("Recovered: $recovered")
 }
