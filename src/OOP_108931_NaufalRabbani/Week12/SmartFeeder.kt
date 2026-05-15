0a56d0587a53a3df500cf5d317c8a2a9687952e5
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-05-15 10:52:31 +0700

    week12: (task) implement food stock validation with throw

--- src/OOP_108931_NaufalRabbani/Week12/SmartFeeder.kt
+++ src/OOP_108931_NaufalRabbani/Week12/SmartFeeder.kt
@@ -9,5 +9,10 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
         throw DispenserJamException()
     }
 
+    if (requestedGram > availableGram){
+        throw FoodEmptyException(requestedGram, requestedGram)
+    }
+
+    return availableGram - requestedGram
 
 }
