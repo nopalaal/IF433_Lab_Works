2c5727f9f76bf20d6fceccd75df81ce04546800f
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-05-15 10:49:54 +0700

    week12: (task) implement hardware validation with throw

--- src/OOP_108931_NaufalRabbani/Week12/SmartFeeder.kt
+++ src/OOP_108931_NaufalRabbani/Week12/SmartFeeder.kt
@@ -1,7 +1,13 @@
 package OOP_108931_NaufalRabbani.Week12
 
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int{
-    require(requestedGram > 1){
+    require(requestedGram > 0){
         "porsi Kibble harus lebih dari 0 gr"
     }
+
+    if (isJammed){
+        throw DispenserJamException()
+    }
+
+
 }
