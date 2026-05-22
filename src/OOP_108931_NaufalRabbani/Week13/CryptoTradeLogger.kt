15ada8c5b00294472bc348093fa9a11bd1822511
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-05-22 21:03:20 +0700

    week13: (task) calculate total PnL from loaded trades

--- src/OOP_108931_NaufalRabbani/Week13/CryptoTradeLogger.kt
+++ src/OOP_108931_NaufalRabbani/Week13/CryptoTradeLogger.kt
@@ -41,4 +41,8 @@ fun main(){
 
     File("crypto_trades.csv").appendText("CORRUPT_ID,DOGEUSDT,Hold,XX,YY\n")
     println("Data korup berhasil diinjeksi")
+
+    val loadedData = loadTrades("crypto_trades.csv")
+    val totalPnl = loadedData.sumOf { it.pnl }
+    println("Total PnL Bersih: $totalPnl")
 }
