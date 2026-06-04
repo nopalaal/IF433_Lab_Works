e7a6c403d68a845f0e5563f7f4c2741d59aac5b4
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-06-04 17:24:07 +0700

    week14: (task) refactor OrderProcessor applying SRP and DIP

--- src/OOP_108931_NaufalRabbani/Week14/EcommerceTask.kt
+++ src/OOP_108931_NaufalRabbani/Week14/EcommerceTask.kt
@@ -1,6 +1,29 @@
 package OOP_108931_NaufalRabbani.Week14
 import java.io.File
 
+interface OrderRepository {
+    fun saveOrder(itemName: String, finalPrice: Double, customerType: String)
+}
+
+class CsvOrderRepository(private val filePath: String = "orders.csv") : OrderRepository {
+    override fun saveOrder(itemName: String, finalPrice: Double, customerType: String) {
+        File(filePath).bufferedWriter().use { writer ->
+            writer.appendLine("$itemName - $finalPrice - $customerType")
+        }
+    }
+}
+
+
+interface NotificationService {
+    fun sendNotification(itemName: String)
+}
+
+class EmailNotifier : NotificationService {
+    override fun sendNotification(itemName: String) {
+        println("Email Terkirim: Pesanan $itemName anda telah dikonfirmasi")
+    }
+}
+
 class BadOrderProcessor{
 
     private val file = File("orders.csv")
