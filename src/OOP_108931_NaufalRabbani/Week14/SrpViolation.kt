2a6c43e585190f4d77c4b38a8e6b53bed2f80240
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-06-04 16:46:24 +0700

    week13: implement basic writeText function

--- src/OOP_108931_NaufalRabbani/Week14/SrpViolation.kt
+++ src/OOP_108931_NaufalRabbani/Week14/SrpViolation.kt
@@ -1,2 +1,18 @@
 package OOP_108931_NaufalRabbani.Week14
 
+data class User(val name: String, val email: String, val age: Int)
+
+class UserManager(){
+    fun validateUser(user: User): Boolean {
+        return user.email.contains("@") && user.age >= 10
+    }
+
+    fun saveUserToDatabase(user: User) {
+        println("INSERT INTO users VALUES (${user.name}, ${user.email})")
+
+    }
+
+    fun sendWelcomeEmail(user: User) {
+        println("Sending Welcome Email")
+    }
+}
