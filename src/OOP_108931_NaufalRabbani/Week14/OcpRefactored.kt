783865d39f9bc1c7b901ee55ccca4619888099dc
nopalaal <naufal.rabbani@student.umn.ac.id>
2026-06-04 16:56:41 +0700

    week14: create DiscountStrategy interface and implementations

---
+++ src/OOP_108931_NaufalRabbani/Week14/OcpRefactored.kt
@@ -0,0 +1,13 @@
+package OOP_108931_NaufalRabbani.Week14
+
+interface DiscountStrategy{
+    fun apply(price:Double):Double
+}
+
+class StudentDiscount : DiscountStrategy{
+    override fun apply(price:Double) = price * 0.80
+}
+
+class MemberDiscount : DiscountStrategy{
+    override fun apply(price:Double) = price * 0.85
+}
